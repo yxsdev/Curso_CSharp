d73530fc97c3fa0f93be38060fb726146d6bb034
yxsdev <yasmindantasbusiness@gmail.com>
2025-01-22 22:10:50 -0300

    Novos exercícios: ex016 e ex017.

---
+++ ex016/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Faça a tabuada de um número que o usuário escolher, só que utilizando um laço for.
+
+            int num1, mult;
+
+            Console.Write("Digite um número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                mult = num1 * i;
+                Console.WriteLine("{0} x {1} = {2}", num1, i, mult);
+            }
+        }
+    }
+}
---
+++ ex017/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ex017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // While (expressão lógica). Só vai executar se a expressão for verdadeira.
+            // Faça um programa que leia o gênero de uma pessoa, mas só aceite os valor "M" ou "F". Caso esteja errado, peça para digitar novamente.
+
+            string genero;
+
+            Console.Write("Digite o gênero: ");
+            genero = Console.ReadLine().ToUpper();
+
+            //ToUpper() - Converte a string para maiúsculo. Função somente para string.
+
+            while (genero != "M" && genero != "F")
+            {
+                Console.Write("Esse gênero não é válido, digite o gênero novamente: ");
+                genero = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine("Bem-vindo(a) ao curso de C#!");
+        }
+    }
+}
