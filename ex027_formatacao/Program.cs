6f1f3482b89ab2386fb723711f067eb47c793eaf
yxsdev <yasmindantasbusiness@gmail.com>
2025-01-29 20:19:08 -0300

    Exercício de formatação de números.

---
+++ ex027_formatacao/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ex027_formatacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 10.54823;
+            double num2 = 7890.98756;
+
+            // Formatação de números
+
+            // Formatação de números para reduzir casas decimais
+            Console.WriteLine($"{num1:F2}");
+
+            // Formatação de números com separador de milhar
+            Console.WriteLine($"{num2:N2}");
+
+            // Formatação de números para moeda(Real pois está em português)
+            Console.WriteLine($"{num1:C2}");
+
+            // Formatação de números para exponencial
+            Console.WriteLine($"{num2:E2}");
+
+            // Formatação de números para porcentagem
+            Console.WriteLine($"{num1:P2}");
+        }
+    }
+}
