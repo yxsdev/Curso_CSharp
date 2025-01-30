3e0fa6b1834cb29998d1e83aaa778a29462f1958
yxsdev <yasmindantasbusiness@gmail.com>
2025-01-29 21:28:13 -0300

    Novos exercícios 29 e 30 realizados sozinha.

---
+++ ex029_areadotriangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ex029_areadotriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a base do triângulo: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Digite a altura do triângulo: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            areaT(b, a);
+        }
+
+        static void areaT(double b, double a)
+        {
+            double t = (b * a) / 2;
+            Console.WriteLine($"A área do triângulo {b} x {b} / 2 é de: {t}.");
+        }
+    }
+}
---
+++ ex030_calculadora/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ex030_calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro valor: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            soma(a, b);
+            multiplicacao(a, b);
+            subtracao(a, b);
+            divisao(a, b);
+        }
+
+        static void soma(int a, int b)
+        {
+            int r = a + b;
+            Console.WriteLine($"{a} + {b} = {r}");
+        }
+
+        static void multiplicacao(int a, int b)
+        {
+            int r = a * b;
+            Console.WriteLine($"{a} x {b} = {r}");
+        }
+
+        static void subtracao(int a, int b)
+        {
+            int r = a - b;
+            Console.WriteLine($"{a} - {b} = {r}");
+        }
+
+        static void divisao(int a, int b)
+        {
+            int r = a / b;
+            Console.WriteLine($"{a} / {b} = {r}");
+        }
+    }
+}
+
